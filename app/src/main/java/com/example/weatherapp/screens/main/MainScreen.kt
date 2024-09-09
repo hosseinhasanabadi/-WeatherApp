e4a525b6544b9be46f8e5e34ced52d4443acbf37
Hossein <hhasnabadi@yahoo.com>
2024-09-09 19:28:58 +0330

    SplashScreen,

---
+++ app/src/main/java/com/example/weatherapp/screens/main/MainScreen.kt
@@ -0,0 +1,41 @@
+package com.example.weatherapp.screens.main
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.weatherapp.data.DataOrException
+import com.example.weatherapp.model.Weather
+
+@Composable
+fun MainScreen(
+    navController: NavController,
+    mainViewModel: MainViewModel = hiltViewModel(),
+) {
+    ShowData(mainViewModel, )
+}
+
+@SuppressLint("ProduceStateDoesNotAssignValue")
+@Composable
+fun ShowData(mainViewModel: MainViewModel) {
+    val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
+        initialValue = DataOrException(loading = true)
+    ) {
+        value = mainViewModel.getWeatherData("Seattle")
+    }.value
+
+    if (weatherData.loading==true){
+        CircularProgressIndicator()
+
+
+    }else if (weatherData.data!=null){
+        Text(text = "main Screen ${weatherData.data.city.country}")
+    }
+
+
+
+}
